db41564dc63a319092d2aff348c59be8cd9d91e0
lnxexu <kcorpuz_220000002183@uic.edu.ph>
2023-12-22 03:25:41 +0800

    Dec 22(small updates)

--- javascript/confirmation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // You can fetch booking details from a server or local storage
-    // and display them on the confirmation page.
-    // For this example, we'll assume data is available in localStorage.
-    const bookingData = localStorage.getItem('bookingData');
-    if (bookingData) {
-        document.getElementById('booking-details').textContent = bookingData;
-    }
-});
